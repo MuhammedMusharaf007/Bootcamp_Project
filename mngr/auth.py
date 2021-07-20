fe974b7740206768b247dd59d6d42447c642d76d
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-20 23:19:32 +0530

    Added Login route to authentication Blueprint

--- mngr/auth.py
+++ mngr/auth.py
@@ -3,6 +3,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.user_agent import UserAgent
 from mngr.db import get_db
 
 bp = Blueprint('auth',__name__, url_prefix='/auth')
@@ -34,3 +35,29 @@ def register():
             return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')    
