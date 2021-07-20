41bde03d7a4730e687da505d9d1156794915ac8e
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-20 23:29:49 +0530

    Added Logout route

--- mngr/auth.py
+++ mngr/auth.py
@@ -61,3 +61,21 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')    
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id)
+        ).fetchone()
+
+
+@bp.route('logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
