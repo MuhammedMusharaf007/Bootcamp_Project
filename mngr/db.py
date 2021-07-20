d44d30449f8cdaef00ea87c8f84c086dac52ddde
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-20 13:48:02 +0530

    Relocated Schema.sql to mngr directory

--- mngr/db.py
+++ mngr/db.py
@@ -21,7 +21,7 @@ def close_db(e=None):
 
 def init_db():
     db=get_db()
-    with current_app.open_resource('schema.sql') as f:
+    with current_app.open_resource('mngr/schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
 
