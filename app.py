db5a2d7c9b406d1dce9ba2d7a41aa9e505ab2a56
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-24 12:58:40 +0530

    Registered Manager blueprint in app.py

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def create_app(test_config=None):
     from mngr import auth
     app.register_blueprint(auth.bp)
     
+    from mngr import manager
+    app.register_blueprint(manager.bp)
+    app.add_url_rule('/', endpoint = 'index')
+    
     return app
     
