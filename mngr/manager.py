c10b0263cdb3b4ee3c2619779a9905a0393761b2
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-25 11:24:32 +0530

    Added getnote function

--- mngr/manager.py
+++ mngr/manager.py
@@ -1,3 +1,4 @@
+from typing_extensions import NotRequired
 from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
 from mngr.auth import login_required
@@ -49,7 +50,7 @@ def create():
             ' WHERE title = ? AND userid = ?',(title, g.user['id']))
             for tag in tags:
                 tag_id = db.execute('SELECT id FROM tags'
-                    ' WHERE name = ?',(tag))
+                    ' WHERE name = ?',(tag,))
                 db.execute(
                     'INSERT INTO tags_notes (notes, tags)'
                     ' VALUES (?, ?)',(note_id, tag_id)
@@ -59,3 +60,17 @@ def create():
     return render_template('manager/create.html')
     
                 
+def get_note(id, check_author=True):
+    note = get_db().execute(
+        'SELECT n.id, n.userid, n.created, n.title, n.body,'
+        ' u.id, u.username,'
+        ' t.name,'
+        'FROM notes n, user u, tags t, tags_notes tn'
+        ' WHERE n.userid = u.id AND t.id = tn.tags AND n.id = tn.notes'
+        'AND n.id = ?',(id,) 
+    ).fetchone()
+    if note is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    if check_author and note['userid'] != g.user['id']:
+        abort(403)
+    return note
