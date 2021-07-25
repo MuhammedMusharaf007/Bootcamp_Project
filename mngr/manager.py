a6b9545f9e7f81970460ba431f55b611db7625f5
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-07-25 11:57:42 +0530

    Update Notes function added

--- mngr/manager.py
+++ mngr/manager.py
@@ -18,7 +18,7 @@ def index():
         ' WHERE n.userid = u.id AND t.id = tn.tags AND n.id = tn.notes'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('notes/index.html', notes=notes)
+    return render_template('manager/index.html', notes=notes)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -50,7 +50,7 @@ def create():
             ' WHERE title = ? AND userid = ?',(title, g.user['id']))
             for tag in tags:
                 tag_id = db.execute('SELECT id FROM tags'
-                    ' WHERE name = ?',(tag,))
+                    ' WHERE name = ?',(tag,)).fetchone()
                 db.execute(
                     'INSERT INTO tags_notes (notes, tags)'
                     ' VALUES (?, ?)',(note_id, tag_id)
@@ -74,3 +74,41 @@ def get_note(id, check_author=True):
     if check_author and note['userid'] != g.user['id']:
         abort(403)
     return note
+
+
+@bp.route('/<int:id>/update', methods = ('GET', 'POST'))
+@login_required
+def update(id):
+    note = get_note(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        tags = request.form['tags']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute('UPDATE notes SET title = ?, body = ? WHERE id = ?',(title, body, id))
+            db.commit()
+            db.execute('DELETE FROM tags_notes WHERE tags_notes.notes = notes.?',(id,))
+            for tag in tags:
+                db.execute(
+                    'INSERT INTO tags (name)'
+                    ' VALUES (?)',(tag)
+                )
+            db.commit()
+            for tag in tags:
+                tag_id = db.execute('SELECT id FROM tags'
+                    ' WHERE name = ?',(tag,)).fetchone()
+                db.execute(
+                    'INSERT INTO tags_notes (notes, tags)'
+                    ' VALUES (?, ?)',(id, tag_id)
+                )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('manager/update.html', note = note)
+    
